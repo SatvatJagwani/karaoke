7fd15d7af9802b4c6b630081cb47a99a5a0c8b2d
Matthew Hutchinson <mshutch@mit.edu>
2018-05-04 12:26:54 -0400

    Test files created

---
+++ test/karaoke/WebServerTest.java
@@ -0,0 +1,16 @@
+package karaoke;
+
+import org.junit.Test;
+
+public class WebServerTest {
+
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    // TODO: Testing Strategy
+    
+    // TODO: Other Tests
+    
+}
---
+++ test/karaoke/parser/PieceParserTest.java
@@ -0,0 +1,16 @@
+package karaoke.parser;
+
+import org.junit.Test;
+
+public class PieceParserTest {
+
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    // TODO: Testing Strategy
+    
+    // TODO: Other Tests
+    
+}
